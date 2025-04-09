6a0af998081a973b5c3ad6c4b1267d4b24a12cc2
Nazar <nkinah93@gmail.com>
2025-04-09 23:08:54 +0300

    Initial commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,26 @@
+import "./style.css";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header__wrapper">
+                <h1 className="header__title">
+                    <strong>Hi, I'm <em>Nazarii Kinashchuk</em></strong>
+                    <br />frontend developer
+                </h1>
+                <div className="header__text">
+                    <p>with passion for learning and creating.</p>
+                </div>
+                <a 
+                    href="/Nazar Kinashchuk.pdf" 
+                    className="btn"
+                    download="Nazar Kinashchuk.pdf"
+                >
+                    Download CV
+                </a>
+            </div>
+        </header>
+    );
+}
+
+export default Header; 
