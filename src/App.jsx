385fe272409ba2d14dd852d86b89ba9c0aecc236
Nazar <nkinah93@gmail.com>
2025-04-09 23:49:52 +0300

    Rename CV file and update download link

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./styles/main.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
@@ -11,17 +11,16 @@ import ScrollToTop from "./utils/scrollToTop";
 function App() {
     return (
         <div className="App">
-            <Router>
-                <ScrollToTop />
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/projects" element={<Projects />} />
-                    <Route path="/project/:id" element={<Project />} />
-                    <Route path="/contacts" element={<Contacts />} />
-                </Routes>
-                <Footer />
-            </Router>
+            <ScrollToTop />
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Navigate to="/home" replace />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/project/:id" element={<Project />} />
+                <Route path="/contacts" element={<Contacts />} />
+            </Routes>
+            <Footer />
         </div>
     );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,21 @@
 import "./style.css";
+import { useEffect } from "react";
 
 const Header = () => {
+    useEffect(() => {
+        const handleDownload = () => {
+            window.open("https://nkinah97.github.io/React-Portfolio/Nazar-Kinashchuk.pdf", "_blank");
+        };
+
+        const downloadBtn = document.querySelector(".btn");
+        if (downloadBtn) {
+            downloadBtn.addEventListener("click", handleDownload);
+            return () => {
+                downloadBtn.removeEventListener("click", handleDownload);
+            };
+        }
+    }, []);
+
     return (
         <header className="header">
             <div className="header__wrapper">
@@ -11,13 +26,9 @@ const Header = () => {
                 <div className="header__text">
                     <p>with passion for learning and creating.</p>
                 </div>
-                <a 
-                    href="/Nazar Kinashchuk.pdf" 
-                    className="btn"
-                    download="Nazar Kinashchuk.pdf"
-                >
+                <button className="btn">
                     Download CV
-                </a>
+                </button>
             </div>
         </header>
     );
