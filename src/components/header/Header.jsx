865f6892b117eb52c37b2ebc18c84df0bec7b452
Nazar <nkinah93@gmail.com>
2025-04-10 21:09:45 +0300

    Повернуто попереднє посилання на резюме

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
                     <p>with passion for learning and creating.</p>
                 </div>
                 <a 
-                    href="./Nazar-Kinachchuk.pdf"
+                    href="https://nkinah97.github.io/React-Portfolio/Nazar-Kinachchuk.pdf"
                     className="btn"
                     target="_blank"
                     rel="noopener noreferrer"
