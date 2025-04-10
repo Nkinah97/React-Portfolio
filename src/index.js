71aa9b4e0ba663cf8e212aa4c58edd43176acbf2
Nazar <nkinah93@gmail.com>
2025-04-10 19:43:10 +0300

    Оновлено налаштування для нового репозиторію

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/React-Portfolio">
+    <BrowserRouter basename="/PortfolioReact">
       <App />
     </BrowserRouter>
   </React.StrictMode>
